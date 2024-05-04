369cd73e93e35cfab9b5f0df5e0491bd15205f99
rala-rationallife <s.tsuduki26@gmail.com>
2024-05-04 23:24:26 +0900

    Add next auth and prisma adapter

---
+++ app/_lib/next-auth/options.ts
@@ -0,0 +1,26 @@
+import prisma from "@lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+};

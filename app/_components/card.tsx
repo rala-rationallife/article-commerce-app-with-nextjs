95bcf7e65d230a568f96b6c7768db150b9b20f2a
rala-rationallife <s.tsuduki26@gmail.com>
2024-05-14 05:39:50 +0900

    Add stripe checkout function

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -1,17 +1,47 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@styles/card.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface Props {
-  url: string;
   image: string;
   title: string;
   price: number;
+  id: string;
 }
 
-export function Card({ url, image, title, price }: Props) {
+export function Card({ image, title, price, id }: Props) {
+  const { data: session } = useSession();
+  const user = session?.user;
+  const router = useRouter();
+
+  const startCheckout = async (articleId: string) => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          articleId,
+          title,
+          price,
+        }),
+      });
+
+      const resData = await res.json();
+
+      if (resData) {
+        router.push(resData.checkout_url);
+      }
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Link href={url} className={styles.card}>
+    <div className={styles.card}>
       <Image
         priority
         src={image}
@@ -21,7 +51,12 @@ export function Card({ url, image, title, price }: Props) {
         className={styles.photo}
       />
       <h2 className={styles.title}>{title}</h2>
-      <p className={styles.price}>{`${price} 円`}</p>
-    </Link>
+      <p className={styles.price}>{`￥ ${price}`}</p>
+      {user && (
+        <button className={styles.button} onClick={() => startCheckout(id)}>
+          購入する
+        </button>
+      )}
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,19 @@ export default async function Home() {
     <main className={styles.main}>
       <div className={styles.grid12}>
         <Card
-          url="/"
           image="/javascript-programming-basics.jpg"
           title="タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル"
           price={2000}
+          id="aaa"
         />
         {contents.map((content) => {
           return (
             <Card
               key={content.id}
-              url="/"
               image={content.thumbnail.url}
               title={content.title}
               price={content.price}
+              id={content.id}
             />
           );
         })}
